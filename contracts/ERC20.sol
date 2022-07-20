c984594f5e45d90474d1b825addb1b193c8d32eb
arturtarasov <artur-tarasov@mail.ru>
2022-07-20 22:04:33 +0300

    added shop

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -99,6 +99,54 @@ contract ERC20 is IERC20 {
     ) internal virtual {}
 }
 
-contract ArturToken is ERC20 {
-    constructor(address shop) ERC20("ArturToken", "ART", 100, shop) {}
+contract ARTURToken is ERC20 {
+    constructor(address shop) ERC20("ARTURToken", "ART", 100, shop) {}
+}
+
+contract ARTShop {
+    IERC20 public token;
+    address payable public owner;
+    event Bought(uint _amount, address indexed _buyer);
+    event Sold(uint _amount, address indexed _seller);
+
+    constructor() {
+        token = new ARTURToken(address(this));
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not an owner!");
+        _;
+    }
+
+    function sell(uint _amountToSell) external {
+        require(
+            _amountToSell > 0 &&
+            token.balanceOf(msg.sender) >= _amountToSell,
+            "incorrect amount!"
+        );
+
+        uint allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= _amountToSell, "check allowance!");
+
+        token.transferFrom(msg.sender, address(this), _amountToSell);
+
+        payable(msg.sender).transfer(_amountToSell);
+
+        emit Sold(_amountToSell, msg.sender);
+    }
+
+    receive() external payable {
+        uint tokensToBuy = msg.value; // 1 wei = 1 token
+        require(tokensToBuy > 0, "not enough funds!");
+
+        require(tokenBalance() >= tokensToBuy, "not enough tokens!");
+
+        token.transfer(msg.sender, tokensToBuy);
+        emit Bought(tokensToBuy, msg.sender);
+    }
+
+    function tokenBalance() public view returns(uint) {
+        return token.balanceOf(address(this));
+    }
 }
